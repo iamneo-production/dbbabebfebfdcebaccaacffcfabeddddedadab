18c51a17bcf9b8839096b484efadee20dc5df830
Iamneo <techadmin@iamneo.ai>
2023-08-07 07:27:30 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Search from './components/Search/Search';
 import './App.css';
-
 function App() {
   return (
     <div>
-      <Search />
+      <Search/>
     </div>
   );
 }
